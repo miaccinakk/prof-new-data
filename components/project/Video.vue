1e03c8878985fa9fc0df5de1895bcdbe6ed7db2c
lessonscourses <lessonscourses@proton.me>
2026-04-25 11:10:07 +0300

    video

--- components/project/Video.vue
+++ components/project/Video.vue
@@ -16,21 +16,28 @@ watch(
   (item) => {
     plyr();
     visible.value = item;
-  }
+  },
 );
+
 watch(visible, (input) => {
   visible.value = input;
 });
 const plyr = async () => {
+  if (!document.querySelector('link[href*="plyr"]')) {
+    const link = document.createElement("link");
+    link.rel = "stylesheet";
+    link.href = "https://cdn.plyr.io/3.7.8/plyr.css";
+    document.head.appendChild(link);
+  }
   const Plyr = await import("plyr");
-  const newPlayer = new Plyr.default("#y" + objectVideo.value);
+  const newPlayer = new Plyr.default("#y" + objectVideo.value.yootube);
 };
 </script>
 <template>
   <ClientOnly>
     <div v-if="visible == true" class="video-item-project">
       <div
-        :id="'y' + objectVideo"
+        :id="'y' + objectVideo.yootube"
         data-plyr-provider="youtube"
         :data-plyr-embed-id="objectVideo"
       ></div>
